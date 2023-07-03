8d4979191a5cd5ab0d7870cfc8f492c91072da87
lucaskahlil <kahlilpereira@gmail.com>
2023-07-03 11:32:51 -0300

    style: edit style sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import '../styles/components/sidebar.sass'
 
 export default function SideBar() {
   return (
-    <div>
-        
-    </div>
+    <aside id='sidebar'>
+        <img src="https://avatars.githubusercontent.com/u/101431445?v=4" alt="Foto Lucas Kahlil" />
+        <p>Desenvolvedor Front-End | Full Stack</p>
+        <p>Redes Sociais</p>
+        <p>Informações de contato</p>
+        <a href="">Download Currículo</a>
+    </aside>
   )
 }
