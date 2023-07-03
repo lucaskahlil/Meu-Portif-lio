a5b7968ce30f7dabf9ba5d781efa04a4548ce74c
lucaskahlil <kahlilpereira@gmail.com>
2023-07-03 16:09:36 -0300

    fit: add section profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import '../styles/components/profile.sass'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+   <section>
+    <h2>Sobre</h2>
+    <p>Sou um desenvolvedor front-end experiente e dedicado, com habilidades sólidas em HTML, CSS, JavaScript, TypeScript e React. Meu objetivo é criar interfaces atraentes e interativas, garantindo a melhor experiência de usuário possível. Mantenho-me atualizado com as tendências e práticas mais recentes no campo do desenvolvimento front-end, sempre buscando aprimorar minhas habilidades. Valorizo a colaboração e estou comprometido em entregar resultados de alta qualidade.</p>
+   </section>
   )
 }
