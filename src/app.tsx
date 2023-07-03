e16e14ba8ec3620fdc075294921fa49a80e4a2e8
lucaskahlil <kahlilpereira@gmail.com>
2023-07-03 10:38:21 -0300

    install sass

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+        <h1>Meu portifólio</h1>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+import './styles/main.sass';
+import App from './app';
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +10,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    
+    < App />
   </React.StrictMode>
 );
